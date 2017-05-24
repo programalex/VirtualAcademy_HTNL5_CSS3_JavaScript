797c9eaa95587024c0aac133fdbea698f76f44ce
alexander lopez <alexander lopez>
2017-05-23 22:14:44 -0500

    subir parte 4

---
+++ partecuatro/javascript.js
@@ -0,0 +1,19 @@
+var ops = {
+multiply: function multiplyNumbers(n1, n2){
+return n1*n2;
+}
+}
+	var x = ops.multiply (2,5);
+
+
+function add (n1,n2){
+    return n1 + n2 ;
+}
+
+function calcular (n1,n2,calculaproceso){
+    return calculaproceso(n1,n2);
+
+}
+function ejecutar (){
+    setOutput (calcular(4,4,add));
+}
